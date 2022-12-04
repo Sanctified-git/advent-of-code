c9b1f692bd42f58314e0e9d8e32165f1230a2864
Sanctified-git <quentin.melis@gmail.com>
2022-12-04 18:26:05 +0100

    Simplify day4 script

--- 2022/src/day4.py
+++ 2022/src/day4.py
@@ -8,15 +8,15 @@ def day4():
     
     for l in input:
         pair: list = l.split(',')
-        a: list[int] = [eval(i) for i in pair[0].split('-')]
-        b: list[int] = [eval(i) for i in pair[1].split('-')]
+        a: list[int] = [int(i) for i in pair[0].split('-')]
+        b: list[int] = [int(i) for i in pair[1].split('-')]
 
         ### PART ONE  ###
         if ((a[0] <= b[0] and a[1] >= b[1]) or (a[0] >= b[0] and a[1] <= b[1])):
             include += 1
         
         ### PART TWO  ###
-        if not((a[0] < b[0] and a[1] < b[0]) or (a[0] > b[0] and a[0] > b[1])):
+        if not(a[1] < b[0] or a[0] > b[1]):
             overlap += 1
 
     print(f"There are {include} assignment pairs where one range includes the other")
