6a458c89196f8ce19c4fa20dab16bbaca7af7d73
Sanctified-git <32301784+Sanctified-git@users.noreply.github.com>
2022-12-13 11:07:28 +0100

    cleanup day 12

--- 2022/src/day12.py
+++ 2022/src/day12.py
@@ -3,6 +3,11 @@ import numpy as np
 from queue import Queue
 from utils.io import get_input
 
+E_COST = 69
+S_COST = 83
+MIN_COST = 96 # 96 is ord(a) - 1
+MAX_COST = 123 # 123 is ord(z) + 1
+
 
 def letter(item: tuple) -> str:
     return grid[item[0]][item[1]]
@@ -10,7 +15,7 @@ def letter(item: tuple) -> str:
 
 def allowed(current: int, neighbor: tuple):
     nb_ord = ord(letter(neighbor))
-    delta = 123 - current if nb_ord == 69 else nb_ord - current
+    delta = MAX_COST - current if nb_ord == E_COST else nb_ord - current
     return True if delta <= 1 else False
 
 
@@ -19,8 +24,8 @@ def neighbors(item: tuple) -> list[tuple]:
     current_cost = ord(letter(item))
     h_min, h_max, w_min, w_max = (0, len(grid) - 1, 0, len(grid[0]) - 1)
 
-    if current_cost == 83:
-        current_cost = 96
+    if current_cost == S_COST:
+        current_cost = MIN_COST
     if item[0] > h_min:
         nb = (item[0] - 1, item[1])
         if allowed(current_cost, nb):
@@ -41,11 +46,10 @@ def neighbors(item: tuple) -> list[tuple]:
 
 
 def breadth_first_search(start: tuple, goal: tuple) -> int:
-
+    steps = 0
     frontier = Queue()
     frontier.put(start, 0)
     came_from = {start: None}
-    steps = 0
 
     while not frontier.empty():
         current = frontier.get()
@@ -54,12 +58,12 @@ def breadth_first_search(start: tuple, goal: tuple) -> int:
             while came_from[current]:
                 steps += 1
                 current = came_from[current]
+            break
 
         for next in neighbors(current):
             if next not in came_from:
                 frontier.put(next)
                 came_from[next] = current
-
     return steps
 
 
@@ -71,19 +75,19 @@ if __name__ == "__main__":
         for j in range(len(grid[0])):
             npgrid[i, j] = grid[i][j]
 
-    start: tuple = tuple(x[0] for x in np.where(npgrid == "S"))
+    goal: tuple = tuple(x[0] for x in np.where(npgrid == "E"))
 
     ### PART ONE ###
 
-    goal: tuple = tuple(x[0] for x in np.where(npgrid == "E"))
+    start: tuple = tuple(x[0] for x in np.where(npgrid == "S"))
     print(f"Getting from S to E takes at least {breadth_first_search(start, goal)} steps")
 
     ### PART TWO ###
 
     candidates: list[tuple] = []
-    match = np.where(npgrid == "a")
-    for i in range(len(match[0]) - 1):
-        candidates.append(tuple(x[i] for x in match))
+    matches = np.where(npgrid == "a")
+    for i in range(len(matches[0]) - 1):
+        candidates.append(tuple(x[i] for x in matches))
     steps: set = sorted(set([breadth_first_search(start, goal) for start in candidates]))
     steps.remove(0)
     print(f"Getting from any a to E takes at least {steps[0]} steps")
